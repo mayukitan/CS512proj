85cac8459186eaa34703e9ba28a9c373a01b8080
Le Tang <letang.boon@gmail.com>
2016-03-13 16:39:25 -0500

    Add train and test data

--- retrieval.py
+++ retrieval.py
@@ -18,22 +18,56 @@ def main(project_id):
             'query': (
                   """SELECT repository_name,
                     repository_owner,
-                    type
-                  FROM [gitcopy.2014_1]
-                  WHERE type CONTAINS "PushEvent"
+                    actor,
+                    repository_language
+                  FROM [gitcopy.2013]
+                  WHERE repository_language CONTAINS 'Python'
+                        AND type CONTAINS 'PullRequestEvent'
                   GROUP BY repository_name,
                     repository_owner,
-                    type;""")
+                    actor,
+                    repository_language;""")
+        }
+        query_request2 = bigquery_service.jobs()
+        query_data2 = {
+            'query': (
+                  """SELECT repository_name,
+                    repository_owner,
+                    actor,
+                    repository_language
+                  FROM [gitcopy.2014_1],
+                        [gitcopy.2014_2],
+                        [gitcopy.2014_3]
+                  WHERE repository_language CONTAINS 'Python'
+                        AND type CONTAINS 'PullRequestEvent'
+                  GROUP BY repository_name,
+                    repository_owner,
+                    actor,
+                    repository_language;""")
         }
 
         query_response = query_request.query(
             projectId=project_id,
             body=query_data).execute()
 
-        print('Query Results:')
-        for row in query_response['rows']:
-            print('\t'.join(field['v'] for field in row['f']))
-    
+        with open('training.txt', 'w') as f:
+            f.write("Training Data (Year 2013)\n")
+            s = "repo_name \t repo_owner \t actor \t language\n"
+            f.write(s)
+            for row in query_response['rows']:
+                f.write('\t'.join(field['v'] for field in row['f']) + '\n')
+
+        query_response2 = query_request2.query(
+            projectId=project_id,
+            body=query_data2).execute()
+
+        with open('ground_truth.txt', 'w') as f:
+            f.write("Ground Truth\n")
+            s = "repo_name \t repo_owner \t actor \t language\n"
+            f.write(s)
+            for row in query_response2['rows']:
+                f.write('\t'.join(field['v'] for field in row['f']) + '\n')
+
     
     except HttpError as err:
         print('Error: {}'.format(err.content))
