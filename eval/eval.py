92a21618410bec1b52c3c49a354dd4caf8005626
chplushsieh <chplushsieh@gmail.com>
2016-04-16 04:32:18 -0500

    eval done

--- eval/eval.py
+++ eval/eval.py
@@ -3,48 +3,58 @@ import pandas
 import numpy
 
 
-def get_actors(filename):
-    return pandas.read_csv(
-        filename,
-        sep='\t', index_col=0, header=None)
-
-
-def create_matrixTA(test_actors, actors):
+def create_matrixTA(test_actors, matrixARA):
     matrixTA = pandas.DataFrame(
-        numpy.zeros(shape=(len(test_actors), len(actors))))
+        numpy.zeros(shape=(len(test_actors), len(matrixARA))))
     matrixTA = matrixTA.set_index(test_actors.index.values)
-    matrixTA.columns = actors.index.values
+    matrixTA.columns = matrixARA.index.values
     return matrixTA
 
 
-def read_data(datafile):
-    return pandas.read_csv(
-        datafile, sep='\t', index_col=False)
+def get_matrixTA(filepath):
+    matrixTA = pandas.DataFrame.from_csv(filepath, header=None)
+    return matrixTA
 
 
-def set_relation(matrixTA, datafile):
-    data = read_data(datafile)
-
-    for index, row in data.iterrows():
-        cur_repo = row[constants.REPO]
-        cur_x = row[type_x]
-        if type == constants.LANG:
-            # make sure a repository gets one language only once
-            matrixRX.loc[cur_repo][cur_x] = 1
-        else:
-            matrixRX.loc[cur_repo][cur_x] += 1
+def get_matrixARA(filepath):
+    matrixARA = pandas.DataFrame.from_csv(filepath)
+    return matrixARA
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("datafile")
-parser.add_argument("test_actor_csv")
-parser.add_argument("all_actor_csv")
+parser.add_argument("test_result_csv")
+parser.add_argument("matrixARA_csv")
 args = parser.parse_args()
 
-print(args.datafile)
 
-test_actors = get_actors(args.test_actor_csv)
-actors = get_actors(args.all_actor_csv)
+matrixTA = get_matrixTA(args.test_result_csv)
+matrixARA = get_matrixARA(args.matrixARA_csv)
+# print(matrixTA)
 
-matrixTA = create_matrixTA(test_actors, actors)
-print(matrixTA)
+eval_result = pandas.DataFrame(
+    numpy.zeros(shape=matrixTA.shape, dtype=bool),
+    columns=numpy.arange(1, matrixTA.shape[1]+1))
+eval_result = eval_result.set_index(matrixTA.index.values)
+# print(eval_result)
+
+total_simialrs = matrixTA.shape[0] * matrixTA.shape[1]
+correct_similars = 0
+for test_actor, similars in matrixTA.iterrows():
+    # print('row:', similars)
+    for k, similar in similars.iteritems():
+        # print('similar:', similar)
+        try:
+            if matrixARA.loc[test_actor][similar] > 0:
+                eval_result.loc[test_actor][k] = True
+                correct_similars += 1
+        except KeyError:
+            pass  # print(similar, "doesn't exist in matrixARA")
+
+print(
+    "Out of your total ", total_simialrs, "predictions, ",
+    correct_similars, "of them are indeed co-commiters. ")
+print("That's ", correct_similars / total_simialrs, "percent corrent. ")
+print("See eval_result.csv to know which of them you got wrong. ")
+
+# print(eval_result)
+eval_result.to_csv('eval_result.csv', header=False)
